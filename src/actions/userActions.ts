0fc2c6b0dfff67e3bdba61dd23c2bfd24b171f01
Nikita Semerikov <ipz215_sno@student.ztu.edu.ua>
2025-01-02 20:31:40 +0200

    feat(user): enhance user actions with new methods
    - Added createUser, updateUserProfile, deleteUserAccount, getUserDashboardData, and getUserDetails methods for improved user management.

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -1,9 +1,14 @@
 "use server";
 
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { dbConnect } from "@/config/db";
+import ApplicationModel from "@/models/Application";
+import CompanyModel from "@/models/Company";
+import JobVacancyModel from "@/models/JobVacancy";
 import UserModel, { IUser } from "@/models/User";
 import { ICreateUserResponse, IUserCreate } from "@/types/user";
 import bcrypt from "bcrypt";
+import { getServerSession } from "next-auth";
 
 export async function createUser(
   data: IUserCreate
@@ -44,12 +49,171 @@ export async function getUserById(userId: string) {
   return await UserModel.findById(userId);
 }
 
-export async function updateUser(userId: string, data: Partial<IUser>) {
-  await dbConnect();
-  return await UserModel.findByIdAndUpdate(userId, data, { new: true });
+export async function updateUserProfile(updatedUser: Partial<IUser>) {
+  try {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const user = await UserModel.findByIdAndUpdate(
+      session.user.id,
+      { ...updatedUser },
+      { new: true }
+    ).lean();
+
+    return {
+      success: true,
+      data: user,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message || "Unknown error occurred",
+    };
+  }
+}
+export async function deleteUserAccount() {
+  try {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const result = await UserModel.findByIdAndDelete(session.user.id);
+    if (!result) {
+      return { success: false, error: "Failed to delete user." };
+    }
+
+    return { success: true, message: "User deleted successfully." };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message || "Unknown error occurred",
+    };
+  }
 }
 
-export async function deleteUser(userId: string) {
-  await dbConnect();
-  return await UserModel.findByIdAndDelete(userId);
+export async function getUserDashboardData() {
+  try {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const user = await UserModel.findById(session.user.id).lean();
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    let company = null;
+    let applications = [];
+
+    if (user.role === "employer") {
+      company = await CompanyModel.findOne({ owner: user._id }).lean();
+
+      if (company && company.jobVacancies) {
+        const jobVacanciesIds = company.jobVacancies.map((vacancy) =>
+          vacancy._id.toString()
+        );
+        applications = await ApplicationModel.find({
+          jobVacancy: { $in: jobVacanciesIds },
+        })
+          .populate({
+            path: "jobVacancy",
+            model: JobVacancyModel,
+          })
+          .populate("applicant")
+          .lean();
+      }
+    } else {
+      applications = await ApplicationModel.find({
+        applicant: user._id,
+      })
+        .populate({
+          path: "jobVacancy",
+          model: JobVacancyModel,
+        })
+        .lean();
+    }
+    const plainData = {
+      user: {
+        ...user,
+        _id: user._id.toString(),
+        createdAt: user.createdAt.toISOString(),
+      },
+      company: company
+        ? {
+            ...company,
+            _id: company._id.toString(),
+            owner: company.owner.toString(),
+            createdAt: company.createdAt.toISOString(),
+          }
+        : null,
+      applications: applications.map((application) => ({
+        ...application,
+        _id: application._id.toString(),
+        applicant: application.applicant
+          ? {
+              ...application.applicant,
+              _id: application.applicant._id.toString(),
+            }
+          : null,
+        jobVacancy: {
+          ...application.jobVacancy,
+          _id: application.jobVacancy._id.toString(),
+          createdAt: application.jobVacancy.createdAt.toISOString(),
+        },
+        appliedAt: application.appliedAt.toISOString(),
+      })),
+    };
+
+    return {
+      success: true,
+      data: plainData,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error instanceof Error ? error.message : "Unknown error occurred",
+    };
+  }
+}
+
+export async function getUserDetails() {
+  try {
+    await dbConnect();
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      throw new Error("User not authenticated");
+    }
+
+    const user = await UserModel.findById(session.user.id).lean();
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    return {
+      success: true,
+      data: {
+        _id: user._id.toString(),
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error.message || "Unknown error occurred",
+    };
+  }
 }
